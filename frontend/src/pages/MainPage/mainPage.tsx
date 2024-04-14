b0ec533438c58de4240ad9d0c0ab373fc6e369f6
Kosti403 <anim.myay@gmail.com>
2024-04-14 19:16:55 +0500

    second

--- frontend/src/pages/MainPage/mainPage.tsx
+++ frontend/src/pages/MainPage/mainPage.tsx
@@ -1,11 +1,19 @@
 import Footer from "../Footer/footer";
+import HeaderForMainPage from "./headerForMainPage/headerForMainPage";
 import s from "./mainPage.module.scss";
 export default function MainPage() {
   return (
     <>
+      <HeaderForMainPage />
       <div className="container">
         <div className={s.mainBlock}>
-          <p>Info</p>
+          <h2>
+            Присоединяйтесь к глобальному и современному хранилищу NFT дипломов.
+          </h2>
+        </div>
+        <div className={s.newFormat}>
+          <h2>Твой новый формат хранения диплома</h2>
+          <p>...</p>
         </div>
       </div>
       <Footer />
