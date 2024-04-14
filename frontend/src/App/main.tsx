5513c71fc6d5829297721b568810c21507bf776d
Kosti403 <anim.myay@gmail.com>
2024-04-14 14:10:31 +0500

    rout

--- frontend/src/App/main.tsx
+++ frontend/src/App/main.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./router.tsx";
 import "./index.scss";
+import { ConfigProvider } from "antd";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ConfigProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ConfigProvider>
 );
---
+++ frontend/src/pages/Footer/footer.tsx
@@ -0,0 +1,8 @@
+import s from "./footer.module.scss";
+export default function Footer() {
+  return (
+    <div className={s.footer}>
+      <h1>WASAAAAAAAAAP</h1>
+    </div>
+  );
+}
