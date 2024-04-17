152da43f0d9d0aaa9263be5c66f35693385682f1
Kosti403 <anim.myay@gmail.com>
2024-04-17 22:01:03 +0500

    modalNFT

--- frontend/src/components/AnimatedBox/PersonAnimated.tsx
+++ frontend/src/components/AnimatedBox/PersonAnimated.tsx
@@ -8,14 +8,14 @@ interface AnimatedBoxProps {
 export const PersonAnimated: React.FC<AnimatedBoxProps> = ({ children }) => {
   return (
     <motion.div
-    className="box"
-    initial={{ opacity: 0, scale: 0.5 }}
-    animate={{ opacity: 1, scale: 1 }}
-    transition={{
-      duration: 0.8,
-      delay: 0.5,
-      ease: [0, 0.71, 0.2, 1.01]
-    }}
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
     >
       {children}
     </motion.div>
---
+++ frontend/src/pages/PersonPage/NFTRegist/NFTRegist.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { observer } from "mobx-react-lite";
+import { Select } from "antd";
+import s from "./NFTRegist.module.scss";
+import { registStore } from "../../../store/regist-store";
+import { Button } from "../../../shared/ui/Button/Button";
+
+const { Option } = Select;
+
+const Modal: React.FC = observer(() => {
+  if (!registStore.isVisible) return null;
+
+  return (
+    <div className={s.modalOverlay} onClick={() => registStore.closeModal()}>
+      <div className={s.modalContent} onClick={(e) => e.stopPropagation()}>
+        <button
+          className={s.closeButton}
+          onClick={() => registStore.closeModal()}
+        >
+          <span>X</span>
+        </button>
+        <h2>Создай свой профиль</h2>
+
+        <div className={s.inputGroup}>
+          <input placeholder="ФИО" />
+          <input placeholder="Полное Название университета" />
+          <div className={s.classification}>
+            <div className={s.degree}>
+              <Select defaultValue="Степень" style={{ width: 140 }}>
+                <Option value="bachelor">Бакалавриат</Option>
+                <Option value="master">Магистратура</Option>
+                <Option value="phd">Докторантура</Option>
+              </Select>
+            </div>
+            <div className={s.colour}>
+              <Select defaultValue="Цвет Диплома" style={{ width: 140 }}>
+                <Option value="blue">Синий </Option>
+                <Option value="red">Красный</Option>
+              </Select>
+            </div>
+          </div>
+        </div>
+
+        <Button type="button">Отправить</Button>
+      </div>
+    </div>
+  );
+});
+
+export default Modal;
